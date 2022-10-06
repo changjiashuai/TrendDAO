0fa391003da7c0c01cc57876dff636671ccd8a09
changjiashuai <changjiashuai@gmail.com>
2022-10-07 00:16:28 +0800

    update to React 18 require.

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from "react-dom/client";
 import './styles/index.css';
 import App from './App';
 
 // Import thirdweb provider and Goerli ChainId
-import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import {ChainId, ThirdwebProvider} from '@thirdweb-dev/react';
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Goerli;
 
+const container = document.getElementById("root");
+const root = createRoot(container);
 // Wrap your app with the thirdweb provider
-ReactDOM.render(
-  <React.StrictMode>
-    <ThirdwebProvider desiredChainId={activeChainId}>
-      <App />
-    </ThirdwebProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
+root.render(
+    <React.StrictMode>
+        <ThirdwebProvider desiredChainId={activeChainId}>
+            <App/>
+        </ThirdwebProvider>
+    </React.StrictMode>
 );
