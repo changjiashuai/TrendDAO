e7bbbe68cd8a53f9cbe99215a762ffe1575ef312
changjiashuai <changjiashuai@gmail.com>
2022-10-06 17:54:12 +0800

    init thirdweb sdk.

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,26 @@
-import { useAddress, useMetamask } from '@thirdweb-dev/react';
+import {useAddress, useMetamask} from "@thirdweb-dev/react";
 
 const App = () => {
-  
-  // Use the hooks thirdweb give us.
-  const address = useAddress();
-  const connectWithMetamask = useMetamask();
-  console.log("👋 Address:", address);
 
-    // This is the case where the user hasn't connected their wallet
-  // to your web app. Let them call connectWallet.
-  if (!address) {
+    const address = useAddress();
+    const connectWithMetamask = useMetamask();
+    console.log("👋 Address:", address);
+
+    if (!address) {
+        return (
+            <div className="landing">
+                <h1>🥳Welcome to TrendDAO</h1>
+                <button onClick={connectWithMetamask} className="btn-hero">
+                    Connect your wallet
+                </button>
+            </div>
+        );
+    }
     return (
-      <div className="landing">
-        <h1>Welcome to NarutoDAO</h1>
-        <button onClick={connectWithMetamask} className="btn-hero">
-          Connect your wallet
-        </button>
-      </div>
+        <div className="landing">
+            <h1>👀 wallet connected, now what!</h1>
+        </div>
     );
-  }
-
-  return (
-    <div className="landing">
-      <h1>👀 wallet connected, now what!</h1>
-    </div>
-  );
 };
 
 export default App;
