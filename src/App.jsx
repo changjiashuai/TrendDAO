0b499b198e214ae50c16c06b435816af6f55a02a
changjiashuai <changjiashuai@gmail.com>
2022-10-06 23:15:08 +0800

    fix thirdweb webpack issue.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,58 @@
-import {useAddress, useMetamask} from "@thirdweb-dev/react";
+import {useAddress, useEditionDrop, useMetamask} from "@thirdweb-dev/react";
+import {useState, useEffect} from 'react';
 
 const App = () => {
 
     const address = useAddress();
     const connectWithMetamask = useMetamask();
     console.log("👋 Address:", address);
+
+    // Initialize our editionDrop contract
+    const editionDrop = useEditionDrop("0x1D6288E8C07CEB1cAa82c1BA4fcC856E7536b2D8");
+    // State variable for us to know if user has our NFT.
+    const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+    // isClaiming lets us easily keep a loading state while the NFT is minting.
+    const [isClaiming, setIsClaiming] = useState(false);
+
+    useEffect(() => {
+        // If they don't have a connected wallet, exit!
+        if (!address) {
+            return;
+        }
+
+        const checkBalance = async () => {
+            try {
+                const balance = await editionDrop.balanceOf(address, 0);
+                if (balance.gt(0)) {
+                    setHasClaimedNFT(true);
+                    console.log("🌟 this user has a membership NFT!");
+                } else {
+                    setHasClaimedNFT(false);
+                    console.log("😭 this user doesn't have a membership NFT.");
+                }
+            } catch (error) {
+                setHasClaimedNFT(false);
+                console.error("Failed to get balance", error);
+            }
+        };
+
+        checkBalance();
+
+    }, [address, editionDrop]);
+
+    const mintNft = async () => {
+        try {
+            setIsClaiming(true);
+            await editionDrop.claim("0", 1);
+            console.log(`🌊 Successfully Minted! Check it out on OpenSea: https://testnets.opensea.io/assets/${editionDrop.getAddress()}/0`);
+            setHasClaimedNFT(true);
+        } catch (error) {
+            setHasClaimedNFT(false);
+            console.error("Failed to mint NFT", error);
+        } finally {
+            setIsClaiming(false);
+        }
+    };
 
     if (!address) {
         return (
@@ -16,9 +64,26 @@ const App = () => {
             </div>
         );
     }
+
+    if (hasClaimedNFT) {
+        return (
+            <div className="member-page">
+                <h1>🍪DAO Member Page</h1>
+                <p>Congratulations on being a member</p>
+            </div>
+        );
+    }
+
+    // Render mint nft screen.
     return (
-        <div className="landing">
-            <h1>👀 wallet connected, now what!</h1>
+        <div className="mint-nft">
+            <h1>Mint your free 🍪DAO Membership NFT</h1>
+            <button
+                disabled={isClaiming}
+                onClick={mintNft}
+            >
+                {isClaiming ? "Minting..." : "Mint your nft (FREE)"}
+            </button>
         </div>
     );
 };
